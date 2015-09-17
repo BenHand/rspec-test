dab89a7064b14383a9f9da0bda200f21f853cfc2
Ben Hand <gneeshot@gmail.com>
2015-09-17 13:18:06 -0500

    finished first test, adding xml file

--- spec/lib/wordpress/comments/client_spec.rb
+++ spec/lib/wordpress/comments/client_spec.rb
@@ -1,3 +1,25 @@
+module Wordpress
+  module Comments
+    class Client
+      attr_reader :url
+
+      def initialize(url)
+        @url = url
+      end
+
+    end
+  end
+end
+
 describe Wordpress::Comments::Client do
-  
+
+  describe "#initialize" do
+
+    it "stores a url" do
+      client = Wordpress::Comments::Client.new('http://mashable.com/comments/feed')
+      expect(client.url).to eq 'http://mashable.com/comments/feed'
+    end
+
+  end
+
 end
